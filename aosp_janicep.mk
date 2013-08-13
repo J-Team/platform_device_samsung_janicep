68324bc5ce947883b1ba02c24040e1682575028d
Alberto Pedron <albertop2197@gmail.com>
2013-08-13 13:52:42 +0200

    Update and rename device_janicep.mk to aosp_janicep.mk
    -Some update from AOSP Sources
    -Disabled some cm lines

--- aosp_janicep.mk
+++ aosp_janicep.mk
@@ -1,10 +1,13 @@
+$(call inherit-product, device/samsung/janicep/full_janicep.mk)
+PRODUCT_NAME := aosp_janicep
+
 # Include common makefile
 $(call inherit-product, device/samsung/u8500-common/common.mk)
 
-ifneq ($(TARGET_SCREEN_HEIGHT),800)
+#ifneq ($(TARGET_SCREEN_HEIGHT),800)
 # Call cm.mk because somehow it's not being called!
-$(call inherit-product, device/samsung/janice/cm.mk)
-endif
+#$(call inherit-product, device/samsung/janice/cm.mk)
+#endif
 
 
 LOCAL_PATH := device/samsung/janicep
