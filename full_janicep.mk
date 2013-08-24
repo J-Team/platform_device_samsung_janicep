af403bc7460144b40a1f64bb007def22d690fff9
Alberto Pedron <albertop2197@gmail.com>
2013-08-24 14:54:37 +0200

    Update full_janicep.mk

--- full_janicep.mk
+++ full_janicep.mk
@@ -1,7 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-# ^^ If it doesn't work, use this --> $(call inherit-product, $(SRC_TARGET_DIR)/product/tiny_aosp.mk) ^^
-$(call inherit-product, device/samsung/janicep/full_janicep.mk)
+$(call inherit-product, device/samsung/janicep/device.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_janicep
