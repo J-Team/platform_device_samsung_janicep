33ab67f50ed6ecd13537ba102c49b043f50556a7
Alberto97 <albertop2197@gmail.com>
2014-04-14 23:25:48 +0200

    Remove DeviceSettings

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@ TARGET_RECOVERY_FSTAB := device/samsung/janice/rootdir/fstab.samsungjanice
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/u8500
-TARGET_KERNEL_CONFIG := cyanogenmod_i9070P_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_i9070p_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
 
 # Assert
